a058dbc34219dc9ab8512ca8f0ca93efb2ad5d67
HiepCoder04 <hiepn1479@gmail.com>
2025-02-17 20:21:23 +0700

    update giao diện admin

---
+++ app/Controllers/Admin/DashboardController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Admin;
+class DashboardController{
+    public function index(){
+       
+        require_once "app/Views/layouts/admin/main.php";
+        // echo '123';
+      }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+use Bramus\Router\Router;
+$router=new Router;
+
+$router->get('/',function(){
+    echo'BASE_URL';
+ });
+
+$router->mount('/admin',function() use($router){
+
+    $router->get('/',App\Controllers\Admin\DashboardController::class.'@index');
+    
+
+});
+
+
+
+
+$router->run();
