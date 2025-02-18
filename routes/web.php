b1e2443be1983d10df70a3a148cddc552f0e431a
HiepCoder04 <hiepn1479@gmail.com>
2025-02-18 21:12:29 +0700

    dang ki dang nhap

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,12 @@ $router->get('/', function () {
     echo 'BASE_URL';
 });
 
+//authenication
+$router->get('/login',App\Controllers\Auth\AuthController::class.'@loginForm');
+$router->get('/register',App\Controllers\Auth\AuthController::class.'@registerForm');
+$router->post('/loginPost',App\Controllers\Auth\AuthController::class.'@login');
+$router->post('/registerPost',App\Controllers\Auth\AuthController::class.'@register');
+
 $router->mount('/admins', function () use ($router) {
 
     $router->get('/', App\Controllers\Admin\DashboardController::class . '@index');
