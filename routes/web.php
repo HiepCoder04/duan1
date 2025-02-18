1aacf038f5cd33d9f81d69279f3148c7606ecef9
nguyenduy-art <dn103abc@gmail.com>
2025-02-18 07:40:20 +0700

    sanpham

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,15 @@ $router->mount('/admins',function() use($router){
     $router->post('/{id}/update',App\Controllers\Admin\CategoriesController::class.'@update');
     $router->get('/{id}/delete',App\Controllers\Admin\CategoriesController::class.'@delete');
     });
+
+     //san pham
+     $router->mount('/products',function() use($router){
+
+        $router->get('/',App\Controllers\Admin\ProductController::class.'@index');
+        $router->get('/create',App\Controllers\Admin\ProductController::class.'@create');
+        $router->post('/store',App\Controllers\Admin\ProductController::class.'@store');
+        $router->get('/{id}/edit',App\Controllers\Admin\ProductController::class.'@edit');
+        $router->post('/{id}/update',App\Controllers\Admin\ProductController::class.'@update');
+        $router->post('/delete',App\Controllers\Admin\ProductController::class.'@delete');
+        });
 });
